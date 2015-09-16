1c276d611f1eea89a2f6b331530c397209708511
Prashanth Sams <sams.prashanth@gmail.com>
2015-09-16 12:56:10 +0530

    initial commit

---
+++ src/main/java/org/seleniumworks/App.java
@@ -0,0 +1,13 @@
+package org.seleniumworks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
